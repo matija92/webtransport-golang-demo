20107c0aed86790bb19591f0612700d98d8df180
unknown <matija.trenkovic@gmail.com>
2023-01-16 19:56:58 -0800

    Marking keyframe in filename for server

--- data/extract-frames.sh
+++ data/extract-frames.sh
@@ -11,3 +11,6 @@ for i in {0001..0901}
 do
    ffmpeg -i $FILE.h264 -c:v libx264 -filter:v "select=gte(n\,$i)" -frames:v 1 -f h264 $FRAME_DIR/frame-$i.h264
 done
+
+# File "sample-640.mp4" has only 1 keyframe, and it's the first frame, so we'll mark it for the server
+mv $FRAME_DIR/frame-0001.h264 $FRAME_DIR/frame-0001.key.h264 
